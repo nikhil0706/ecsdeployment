f9b4d6b34a9e182988400ae8bbd7405f38f8cf9f
nikhil0706 <nikhil.devraj77@gmail.com>
2025-03-01 11:56:29 -0600

    Create variables.tf

---
+++ environments/dev/variables.tf
@@ -0,0 +1,36 @@
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+variable "ecs_cluster_name" {
+  default = "my-app-cluster"
+}
+
+variable "ecs_service_name" {
+  default = "my-app-service"
+}
+
+variable "ecr_repository_name" {
+  default = "my-app"
+}
+
+variable "cpu" {
+  default = "512"
+}
+
+variable "memory" {
+  default = "1024"
+}
+
+variable "container_port" {
+  default = 80
+}
+
+variable "vpc_id" {
+  description = "VPC ID where ECS will be deployed"
+}
+
+variable "subnet_ids" {
+  description = "List of subnets for ECS tasks"
+  type        = list(string)
+}
