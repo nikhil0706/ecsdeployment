cec4da174989d9d16469f12e941709e56cfff1df
nikhil0706 <nikhil.devraj77@gmail.com>
2025-03-01 12:35:12 -0600

    Update output.tf

--- environments/dev/output.tf
+++ environments/dev/output.tf
@@ -2,10 +2,6 @@ output "ecs_service_name" {
   value = aws_ecs_service.app_service.name
 }
 
-output "load_balancer_url" {
-  value = data.aws_lb.existing_lb.dns_name
-}
-
 output "task_definition_arn" {
   value = data.aws_ecs_task_definition.existing_task.arn
 }
