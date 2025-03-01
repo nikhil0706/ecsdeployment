8e71a881367105fe641e7c2f2ef451fef21c6503
nikhil0706 <nikhil.devraj77@gmail.com>
2025-03-01 13:01:12 -0600

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,42 +1,42 @@
-variable "aws_region" {
-  default = "us-east-2"
-}
+#variable "aws_region" {
+#  default = "us-east-2"
+#}
 
-variable "ecs_cluster_name" {
-  default = "my-app-cluster"
-}
+#variable "ecs_cluster_name" {
+#  default = "my-app-cluster"
+#}
 
-variable "ecs_service_name" {
-  default = "my-app-service"
-}
+#variable "ecs_service_name" {
+#  default = "my-app-service"
+#}
 
-variable "ecr_repository_name" {
-  default = "my-app"
-}
+#variable "ecr_repository_name" {
+#  default = "my-app"
+#}
 
-variable "cpu" {
-  default = "512"
-}
+#variable "cpu" {
+#  default = "512"
+#}
 
-variable "memory" {
-  default = "1024"
-}
+#variable "memory" {
+#  default = "1024"
+#}
 
-variable "container_port" {
-  default = 80
-}
+#variable "container_port" {
+#  default = 80
+#}
 
-variable "vpc_id" {
-  description = "VPC ID where ECS will be deployed"
-}
+#variable "vpc_id" {
+#  description = "VPC ID where ECS will be deployed"
+#}
 
-variable "subnet_ids" {
-  description = "List of subnets for ECS tasks"
-  type        = list(string)
-}
+#variable "subnet_ids" {
+#  description = "List of subnets for ECS tasks"
+#  type        = list(string)
+#}
 
-variable "task_family_name" {
-  description = "The family name of the ECS task definition"
-  type        = string
-}
+#variable "task_family_name" {
+#  description = "The family name of the ECS task definition"
+#  type        = string
+#}
 
