e6757d569ad1c5c4adb3ea4f6283ffab69d17c95
nikhil0706 <nikhil.devraj77@gmail.com>
2025-03-01 12:33:51 -0600

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -27,26 +27,3 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# ECS Task Definition
-resource "aws_ecs_task_definition" "app_task" {
-  family                   = var.ecs_service_name
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = var.cpu
-  memory                   = var.memory
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-
-  container_definitions = jsonencode([
-    {
-      name      = "app-container"
-      image     = "${data.aws_ecr_repository.app_repo.repository_url}@${data.aws_ecr_image.latest_image.image_digest}"
-      cpu       = var.cpu
-      memory    = var.memory
-      essential = true
-      portMappings = [{
-        containerPort = var.container_port
-        hostPort      = var.container_port
-      }]
-    }
-  ])
-}
