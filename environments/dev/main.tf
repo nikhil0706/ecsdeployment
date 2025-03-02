bbe1955437997c690dbf8c1dd2c8e462eea64a82
nikhil0706 <nikhil.devraj77@gmail.com>
2025-03-02 13:54:00 -0600

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -11,24 +11,24 @@ terraform {
 }
 
 # Retrieve existing ECS cluster
-data "aws_ecs_cluster" "existing" {
-  cluster_name = var.ecs_cluster_name
-}
+#data "aws_ecs_cluster" "existing" {
+#  cluster_name = var.ecs_cluster_name
+#}
 
 # IAM Role for Task Execution
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
+#resource "aws_iam_role" "ecs_task_execution_role" {
+#  name = "ecsTaskExecutionRole"
 
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-    }]
-  })
-}
+#  assume_role_policy = jsonencode({
+#    Statement = [{
+#      Action = "sts:AssumeRole"
+#      Effect = "Allow"
+#      Principal = {
+#        Service = "ecs-tasks.amazonaws.com"
+#      }
+#    }]
+#  })
+#}
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
