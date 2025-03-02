17adb57f6b10f46cbdcf4fba4906e7696e2634fe
nikhil0706 <nikhil.devraj77@gmail.com>
2025-03-02 13:55:54 -0600

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -30,8 +30,8 @@ terraform {
 #  })
 #}
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
+#resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+#  role       = aws_iam_role.ecs_task_execution_role.name
+#  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+#}
 
