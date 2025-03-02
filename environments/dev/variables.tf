8fd3addcf3e7202c392092687c1f2fac07116066
nikhil0706 <nikhil.devraj77@gmail.com>
2025-03-02 13:48:50 -0600

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -12,36 +12,36 @@ variable "ecs_service_name" {
 
 ###############
 
-variable "ecr_repository_name" {
-  description = "The name of the existing ECR repository"
-  type        = string
-}
+#variable "ecr_repository_name" {
+#  description = "The name of the existing ECR repository"
+#  type        = string
+#}
 
 ################
 
-variable "cpu" {
-  default = "512"
-}
+#variable "cpu" {
+#  default = "512"
+#}
 
-variable "memory" {
-  default = "1024"
-}
+#variable "memory" {
+#  default = "1024"
+#}
 
-variable "container_port" {
-  default = 80
-}
+#variable "container_port" {
+#  default = 80
+#}
 
-variable "vpc_id" {
-  description = "VPC ID where ECS will be deployed"
-}
+#variable "vpc_id" {
+#  description = "VPC ID where ECS will be deployed"
+#}
 
-variable "subnet_ids" {
-  description = "List of subnets for ECS tasks"
-  type        = list(string)
-}
+#variable "subnet_ids" {
+#  description = "List of subnets for ECS tasks"
+#  type        = list(string)
+#}
 
-variable "task_family_name" {
-  description = "The family name of the ECS task definition"
-  type        = string
-}
+#variable "task_family_name" {
+#  description = "The family name of the ECS task definition"
+#  type        = string
+#}
 
