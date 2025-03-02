dca491e4a4c0f4db1794b5be0e8b2a8586556a78
nikhil0706 <nikhil.devraj77@gmail.com>
2025-03-02 13:45:58 -0600

    Update output.tf

--- environments/dev/output.tf
+++ environments/dev/output.tf
@@ -1,7 +1,7 @@
-output "ecs_service_name" {
-  value = aws_ecs_service.app_service.name
-}
-
-output "task_definition_arn" {
-  value = data.aws_ecs_task_definition.existing_task.arn
-}
+#output "ecs_service_name" {
+#  value = aws_ecs_service.app_service.name
+#}
+#
+#output "task_definition_arn" {
+#  value = data.aws_ecs_task_definition.existing_task.arn
+#}
